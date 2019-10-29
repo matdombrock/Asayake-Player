96bfc3debfce02125a65c5638eb74b5a593e9baf
Mathieu Dombrock <matdombrock@gmail.com>
2019-10-29 22:21:13 +0000

    Move SC genrator JS logic to own file

--- asayake.php
+++ asayake.php
@@ -8,13 +8,17 @@
  * Author URI: https://mzero.space
  */
 add_action( 'wp_enqueue_scripts', 'aplayer_scripts' );
-add_action( 'wp_enqueue_styles', 'aplayer_styles' );
+add_action( 'admin_enqueue_scripts', 'aplayer_admin_scripts' );
+//add_action( 'wp_enqueue_styles', 'aplayer_styles' );
 function aplayer_scripts(){
     wp_enqueue_script( 'aplayer-js', plugins_url( '/js/player.js', __FILE__ ));
     wp_localize_script( 'aplayer-js', 'phpdata', array(
         'pluginsUrl' => plugins_url('',__FILE__),
     ));
     wp_enqueue_style( 'aplayer-css', plugins_url( '/css/player.css', __FILE__ ));
+}
+function aplayer_admin_scripts(){
+    wp_enqueue_script( 'aplayer-scjs', plugins_url( '/js/scGenerator.js', __FILE__ ));
 }
 
 include( plugin_dir_path( __FILE__ ) . 'options.php');
--- options.php
+++ options.php
@@ -1,4 +1,5 @@
 <?php
+
 // OPTIONS
 /** Step 2 (from text above). */
 add_action( 'admin_menu', 'my_plugin_menu' );
@@ -61,125 +62,7 @@ Type:
 
 	$genScript = "
 <script>
-var cur_type = 'stand-alone';
-var sa_sc = {//stand alone shortcode data
-	playlistID: '',
-	title: '',
-	artist: '',
-	album: '',
-	url: '',
-};
-var p_sc = [];// playlist shortcode data
-function updateType(value){
-	//alert(value);
-	if(cur_type!=value){
-		clearFields();
-		if(value=='playlist'){
-			document.getElementById('agen-id').value = '';
-			p_sc = [];
-		}
-	}
-	cur_type = value;
-	if(value=='stand-alone'){
-		document.getElementById('agen-stand-alone').classList.remove('hidden');
-		document.getElementById('agen-playlist').classList.add('hidden');
-	}else{
-		document.getElementById('agen-stand-alone').classList.add('hidden');
-		document.getElementById('agen-playlist').classList.remove('hidden');
-	}
-	generateSA_SC();
-}
-function updateSA_SC(type,value){
-	//document.getElementById('agen-sc').innerHTML = value;
-	if(type!=null&&value!=null){
-		sa_sc[type] = value;
-	}
-	generateSA_SC();
-}
-function generateSA_SC(){
-	var sc = '';
-	if(cur_type == 'playlist'){
-		sc += '[aplayer-playlist playlist_id=\"'+sa_sc[\"playlistID\"]+'\"]';
-	}else{
-		sc += '[ aplayer ';
-	}
-	if(p_sc.length > 0 && cur_type == 'playlist'){//do many\
-		for (i = 0; i < p_sc.length; i++) {
-			sc += '[ aplayer-playlist-item ';
 
-			sc += 'playlist_id=\"';
-			sc += p_sc[i][\"playlistID\"];
-			sc += '\" ';
-	
-			sc += 'url=\"';
-			sc += p_sc[i][\"url\"];
-			sc += '\" ';
-		
-			sc += 'artist=\"';
-			sc += p_sc[i][\"artist\"];
-			sc += '\" '; 
-		
-			sc += 'album=\"';
-			sc += p_sc[i][\"album\"];
-			sc += '\" '; 
-		
-			sc += 'title=\"';
-			sc += p_sc[i][\"title\"];
-			sc += '\" '; 
-		
-			sc += ']'; 
-		}
-	}
-	//do current
-	if(cur_type == 'playlist'){
-		sc += '[ aplayer-playlist-item ';
-		sc += 'playlist_id=\"';
-		sc += sa_sc[\"playlistID\"];
-		sc += '\" ';
-	}
-	sc += 'url=\"';
-	sc += sa_sc[\"url\"];
-	sc += '\" ';
-
-	sc += 'artist=\"';
-	sc += sa_sc[\"artist\"];
-	sc += '\" '; 
-
-	sc += 'album=\"';
-	sc += sa_sc[\"album\"];
-	sc += '\" '; 
-
-	sc += 'title=\"';
-	sc += sa_sc[\"title\"];
-	sc += '\" '; 
-
-	sc += ']'; 
-
-	document.getElementById('agen-sc').innerHTML = sc;
-}
-function newTrack(){
-	p_sc.push(sa_sc);
-	clearFields();
-	generateSA_SC();
-}
-function clearFields(){
-	document.getElementById('agen-title').value = '';
-	document.getElementById('agen-album').value = '';
-	document.getElementById('agen-artist').value = '';
-	document.getElementById('agen-url').value = '';
-	document.getElementById('agen-title-p').value = '';
-	document.getElementById('agen-album-p').value = '';
-	document.getElementById('agen-artist-p').value = '';
-	document.getElementById('agen-url-p').value = '';
-	sa_sc = {//stand alone shortcode data
-		playlistID: '',
-		title: '',
-		artist: '',
-		album: '',
-		url: '',
-	};
-}
-updateType('stand-alone');
 </script>	
 	";
 
