abc432ffbb1c4b3a8f6df5dedeabc63e4f7c6f47
Mathieu Dombrock <matdombrock@gmail.com>
2019-10-29 22:04:00 +0000

    Add 'safe' variables to escape quotes in meta data

--- shortcodes.php
+++ shortcodes.php
@@ -7,15 +7,21 @@ function aplayerShortcode($atts){
 
     $playerID = "aplayer_".uniqid();
 
+    $safe_url = addslashes($url);
+    $safe_artist = addslashes($artist);
+    $safe_title = addslashes($title);
+    $safe_album = addslashes($album);
+    $safe_playerID = addslashes($playerID);
+    
     $img_playBtn = plugins_url( '/img/play.png', __FILE__ );
     $img_stopBtn = plugins_url( '/img/stop.png', __FILE__ );
 
-    $ret = "<div class='aplayer-wrap' id='$playerID-wrap'>";
+    $ret = "<div class='aplayer-wrap' id='$safe_playerID-wrap'>";
     $ret .= "Loading Player...";
     $ret .= "</div>";
     $ret .= "
     <script>
-        aplayerInstance.generatePlayer('$url', '$title', '$artist', '$album', '$playerID');
+        aplayerInstance.generatePlayer('$safe_url', '$safe_title', '$safe_artist', '$safe_album', '$safe_playerID');
     </script>";
     return $ret;
 }
@@ -23,15 +29,15 @@ function aplayerShortcodePlaylist($atts){
     //$playerID = "aplayer_".uniqid();
     $playerID = $atts['playlist_id'];
     //return $id;
-
+    $safe_playerID = addslashes($playerID);
     //dont think these hget used anymore not sure
     $img_playBtn = plugins_url( '/img/play.png', __FILE__ );
     $img_stopBtn = plugins_url( '/img/stop.png', __FILE__ );
 
-    $ret = "<div class='aplayer-wrap' id='$playerID-wrap'>";
+    $ret = "<div class='aplayer-wrap' id='$safe_playerID-wrap'>";
     $ret .= "Loading Player...";
     $ret .= "</div>";
-    $ret .= "<script>aplayerInstance.generatePlayer('#', '#', '#', '#', '$playerID','true');</script>";
+    $ret .= "<script>aplayerInstance.generatePlayer('#', '#', '#', '#', '$safe_playerID','true');</script>";
     return $ret;
 }
 
@@ -41,7 +47,14 @@ function aplayerShortcodePlaylistItem($atts){
     $title = $atts['title'];
     $artist = $atts['artist'];
     $album = $atts['album'];
-    $ret = "<script>aplayerInstance.addPlaylistItem('$playlistID','$url','$artist','$title','$album');</script>";
+
+    $safe_url = addslashes($url);
+    $safe_artist = addslashes($artist);
+    $safe_title = addslashes($title);
+    $safe_album = addslashes($album);
+    $safe_playlistID = addslashes($playlistID);
+
+    $ret = "<script>aplayerInstance.addPlaylistItem('$safe_playlistID','$safe_url','$safe_artist','$safe_title','$safe_album');</script>";
     return $ret;
 }
 ?>
